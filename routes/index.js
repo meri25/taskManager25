7f2516ecce521eeec823c729707416931a7f8bcb
Meri Takahashi <meri@MacBook-Pro-2.local>
2018-02-08 22:45:49 +0900

    var

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,7 @@ var router = express.Router();
 
 //現在のサーバの環境変数を取得出来る
 //herokuで導入したmongodbのURIを取得
-const MONGO_URI = process.env.MONGO_URI;
-
-
+var MONGO_URI = process.env.MONGO_URI;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
