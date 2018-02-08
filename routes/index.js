83889b86176843c76c10c7afaba320fd70c5403e
Meri Takahashi <meri@MacBook-Pro-2.local>
2018-02-08 22:42:58 +0900

    MONGO

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+//現在のサーバの環境変数を取得出来る
+//herokuで導入したmongodbのURIを取得
+const MONGO_URI = process.env.MONGO_URI;
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: MONGO_URI});
 });
 
 module.exports = router;
